0d3711fa60fd6935b5d5224085ae1a541bb75e09
Chitipolu Sri Sudheera <csrisudheera96@gmail.com>
2021-02-05 10:11:26 -0600

    added user entity

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,22 @@
-export class User {}
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  firstName: string;
+
+  @Column()
+  lastName: string;
+
+  @Column()
+  email: string; 
+  
+  @Column() 
+  password: string;
+
+  @Column({ default: true })
+  isActive: boolean;
+}
